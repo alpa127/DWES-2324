ed3eb446fe8117475b671586a7e534772e8a91ac
alpa127 <alvaropachon028@gmail.com>
2024-01-15 09:05:31 +0000

    commit

--- u4_tiendaLaravel/database/migrations/2024_01_15_084419_create_pedidos_table.php
+++ u4_tiendaLaravel/database/migrations/2024_01_15_084419_create_pedidos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->datetime('fecha');
+            $table->dateTime('fecha');
         });
     }
 
